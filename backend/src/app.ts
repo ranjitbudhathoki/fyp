6e27610a08b87e7d23ef75ccec77fd11092684db
whoisranjit <ranjitbudhathoki07@gmail.com>
2023-01-19 12:56:51 +0545

    bug fixes in github strategy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,34 @@
-import express, {Request, Response} from 'express';
-import passport from 'passport';
-import { Strategy as GithubStrategy  } from 'passport-github';
-const app = express()
+import express, { Request, Response } from "express";
+import passport from "passport";
+import { Strategy as GithubStrategy } from "passport-github";
+const app = express();
 
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      callbackURL: "/auth/github/callback",
+    },
+    (
+      accessToken: string,
+      refreshToken: string,
+      profile: GithubStrategy.Profile,
+      done
+    ) => {
+      console.log("access", accessToken);
+      console.log("refresh", refreshToken);
+      console.log("profile", profile);
+    }
+  )
+);
 
-passport.use(new GithubStrategy({
-    clientID: process.env.GITHUB_CLIENT_ID as string,
-    clientSecret:process.env.GITHUB_CLIENT_SECRET as string,
-    callbackURL:'/auth/github/callback'
-}, (accessToken:string)=>{console.log(accessToken)}));
+app.use(express.json());
 
-app.use(express.json())
+app.get(
+  "/auth/github",
+  passport.authenticate("github", { scope: ["profile", "email"] })
+);
 
-app.get('/auth/github',passport.authenticate('github', {scope:['profile', 'email']}))
+app.get("/auth/github/callback", passport.authenticate("github"));
 export default app;
-
-
-
