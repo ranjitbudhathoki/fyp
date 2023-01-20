4e86dd07fe53b1614c6d2d669e1241d2b5dbc82c
whoisranjit <ranjitbudhathoki07@gmail.com>
2023-01-20 20:50:48 +0545

    bug fixes in protectedroute

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store/authStore";
+import { store } from "./redux/store/authStore";
 
 const container = document.getElementById("root") as HTMLDivElement;
 const root = createRoot(container);
