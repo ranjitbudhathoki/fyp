c4db901328404127bcb792541e339eff1c3eeeaa
whoisranjit <ranjitbudhathoki07@gmail.com>
2023-04-02 21:34:09 +0545

    add handlers in admin controller

--- backend/src/controllers/admin.controller.ts
+++ backend/src/controllers/admin.controller.ts
@@ -2,6 +2,7 @@ import { catchAsync } from '../utils/catchAsnyc';
 import AppError from '../utils/appError';
 import prisma from '../services/prisma';
 import verifyPassword from '../utils/verifyPassword';
+import generateToken from '../utils/generateToken';
 
 const handleAdminLogin = catchAsync(async (req, res, next) => {
   const { username, password } = req.body;
@@ -18,4 +19,94 @@ const handleAdminLogin = catchAsync(async (req, res, next) => {
     return next(new AppError('You are not allowded to login!', 401));
   }
   await verifyPassword(password, admin.password);
+  const token = generateToken({ id: admin.id });
+
+  return res.status(200).json({
+    status: 'success',
+    data: {
+      token,
+      id: admin.id,
+      username: admin.username,
+    },
+  });
 });
+
+const getAllRegisteredUser = catchAsync(async (req, res, next) => {
+  const { page } = req.query;
+  const pageCount = 10;
+  const registeredUser = await prisma.user.findMany({
+    skip: (Number(page) - 1) * pageCount,
+    take: pageCount,
+  });
+
+  return res.status(200).json({
+    status: 'success',
+    data: registeredUser,
+  });
+});
+
+const deRegisterUser = catchAsync(async (req, res, next) => {
+  const { userId } = req.params;
+  const removedUser = await prisma.user.delete({
+    where: { id: userId },
+  });
+
+  return res.status(200).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+const getUserBySearch = catchAsync(async (req, res, next) => {
+  const { searchTerm } = req.params;
+  if (!searchTerm) {
+    return next(new AppError('Please provide search term!', 400));
+  }
+
+  const results = await prisma.user.findMany({
+    where: {
+      OR: [
+        {
+          username: {
+            contains: searchTerm,
+            mode: 'insensitive',
+          },
+        },
+      ],
+    },
+  });
+
+  if (!results) {
+    return next(new AppError('No user found!', 404));
+  }
+
+  return res.status(200).json({
+    status: 'success',
+    data: results,
+  });
+});
+
+const getUserById = catchAsync(async (req, res, next) => {
+  const { userId } = req.params;
+
+  if (!userId) return next(new AppError('Please provide user id', 400));
+
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+  });
+
+  if (!user) return next(new AppError('User not found', 404));
+
+  return res.status(200).json({
+    status: 'success',
+    data: user,
+  });
+});
+
+export {
+  handleAdminLogin,
+  getAllRegisteredUser,
+  deRegisterUser,
+  getUserBySearch,
+  getUserById,
+};
