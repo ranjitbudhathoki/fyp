e0ed5d64b204674dafd612414644297ab5661018
whoisranjit <ranjitbudhathoki07@gmail.com>
2023-04-03 13:45:41 +0545

    add 404 page

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
-function NotFound() {
-  return <div>NotFound</div>;
-}
+const NotFound = () => {
+  return (
+    <div className="h-full flex justify-center items-center ">
+      <h2 className="text-6xl md:text-9xl">404</h2>
+    </div>
+  );
+};
 
 export default NotFound;
