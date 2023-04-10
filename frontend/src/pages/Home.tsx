f79800750ffe365c1103e3ddb2f58baf726ba63c
whoisranjit <ranjitbudhathoki07@gmail.com>
2023-04-10 10:29:57 +0545

    add profile setup form

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,17 +2,31 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import CustomToastify from '../components/CustomToastify';
 import Header from '../components/Header';
-
+import ProfileForm from '../components/Profile/ProfileForm';
 import ProtectedRoute from '../components/ProtectedRoute';
+import { useSelector } from 'react-redux';
+
 const Home: React.FC = () => {
+  const { user } = useSelector((state: any) => state.auth);
+
+  const showProfileSetup =
+    user?.createdAt && user?.updatedAt && user.createdAt === user.updatedAt;
+
   return (
     <ProtectedRoute>
       <CustomToastify />
       <section className=" flex flex-col  h-full">
         <Header />
-        <div className="flex flex-grow ">
+        {showProfileSetup ? (
+          <ProfileForm />
+        ) : (
+          <div className="flex flex-grow ">
+            <Outlet />
+          </div>
+        )}
+        {/* <div className="flex flex-grow ">
           <Outlet />
-        </div>
+        </div> */}
       </section>
     </ProtectedRoute>
   );
