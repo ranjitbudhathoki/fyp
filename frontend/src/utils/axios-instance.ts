fe395402d39f5aeb5b5512286291d7a4d8fb92b2
whoisranjit <ranjitbudhathoki07@gmail.com>
2023-04-12 22:29:38 +0545

    add instance

--- frontend/src/utils/axios-instance.ts
+++ frontend/src/utils/axios-instance.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: "http://localhost:8000",
-  baseURL: process.env.REACT_APP_SERVER_URL,
+  baseURL: 'https://date-now.onrender.com',
   withCredentials: true,
 });
