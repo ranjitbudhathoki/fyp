95f8278a7c15ad3c3ac7dcf3f12b9dca6732a8d3
whoisranjit <ranjitbudhathoki07@gmail.com>
2023-04-12 22:21:37 +0545

    re build

--- frontend/src/api/socket.ts
+++ frontend/src/api/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // const socket = io('http://localhost:8000');
-const socket = io(process.env.REACT_APP_SERVER_URL);
+const socket = io('https://date-now.onrender.com');
 
 export default socket;
--- frontend/src/api/systemAxios.ts
+++ frontend/src/api/systemAxios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: 'http://localhost:8000',
-  baseURL: process.env.REACT_APP_SERVER_URL,
+  baseURL: 'https://date-now.onrender.com',
 
   headers: {
     authorization: `Bearer ${localStorage.getItem('token')}`,
