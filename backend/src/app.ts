b181bf0f96f4960e90aa385c0b463935d10b2235
whoisranjit <ranjitbudhathoki07@gmail.com>
2023-04-12 20:25:39 +0545

    ready for deployment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,6 +15,7 @@ import solutionRouter from './routes/solution.routes';
 import matchRouter from './routes/match.routes';
 import adminRouter from './routes/admin.routes';
 import notificationRouter from './routes/notificaion.routes';
+import path from 'path';
 
 const app = express();
 app.use(express.json());
@@ -51,6 +52,12 @@ app.use('/api/match-posts', matchPostRouter);
 app.use('/api/matches', checkLoggedIn, matchRouter);
 app.use('/api/solutions', checkLoggedIn, solutionRouter);
 app.use('/notification', checkLoggedIn, notificationRouter);
+
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+app.get('/*', (req, res) => {
+  return res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
